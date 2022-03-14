a58b234fc451a6956e2027747a3783b06294311b
Al Algorithmite <algorithmite@gmail.com>
2022-03-14 12:25:34 -0500

    Reformat Route CRUD

--- src/model/api/routes.rs
+++ src/model/api/routes.rs
@@ -3,59 +3,110 @@ use crate::model::database::{
 };
 use rocket_sync_db_pools::diesel::{insert_into, prelude::*, result::QueryResult, PgConnection};
 
-/*
-#[derive(Clone, Debug, Insertable)]
-#[table_name = "routes"]
-pub struct NewRoute<'a> {
-    pub parent: Option<i32>,
-    pub url_slug: &'a str,
-}
-#[derive(Clone, Debug, Eq, Identifiable, PartialOrd, Queryable)]
-pub struct Route {
-    pub id: i32,
-    pub parent: Option<i32>,
-    pub url_slug: String,
-    pub created_at: NaiveDateTime,
-    pub updated_at: NaiveDateTime,
-}
-*/
-
-pub enum RouteCreationStatus {
-    Success(QueryResult<Route>),
-    RouteNameExists,
-}
-
-pub enum RouteUpdateStatus<T> {
-    Success(T),
-    Failure(T),
-    ValueOverlap,
-    DoesNotExist,
-}
-
-pub fn query_id(conn: &PgConnection, query_id: i32) -> QueryResult<Route> {
-    routes.filter(id.eq(query_id)).first(conn)
-}
-
-pub fn query_url_slug(conn: &PgConnection, query_url_slug: String) -> QueryResult<Route> {
-    routes.filter(url_slug.eq(query_url_slug)).first(conn)
-}
-
-pub fn query_all_parent(conn: &PgConnection, query_parent_id: i32) -> QueryResult<Vec<Route>> {
-    routes.filter(parent.eq(query_parent_id)).get_results(conn)
-}
+//Create
 
 pub fn create_user_action(
     conn: &PgConnection,
     input_parent: Option<i32>,
     input_url_slug: String,
-) -> RouteCreationStatus {
-    match query_url_slug(conn, input_url_slug.to_owned()) {
-        Ok(_) => return RouteCreationStatus::RouteNameExists,
+) -> Option<QueryResult<Route>> {
+    match read_route(conn, None, None, Some(input_url_slug.to_owned())) {
+        Ok(_) => return None,
         _ => (),
     };
     let new_route: NewRoute = NewRoute {
         parent: input_parent,
         url_slug: &input_url_slug,
     };
-    RouteCreationStatus::Success(insert_into(routes).values(&new_route).get_result(conn))
+    Some(insert_into(routes).values(&new_route).get_result(conn))
+}
+
+//Read
+
+pub fn read_route(
+    conn: &PgConnection,
+    option_id: Option<i32>,
+    option_parent: Option<Option<i32>>,
+    option_url_slug: Option<String>,
+) -> QueryResult<Route> {
+    let mut query = routes.into_boxed();
+    match option_id {
+        Some(query_id) => query = query.filter(id.eq(query_id)),
+        _ => (),
+    }
+    match option_parent {
+        Some(query_parent) => query = query.filter(parent.eq(query_parent)),
+        _ => (),
+    }
+    match option_url_slug {
+        Some(query_url_slug) => query = query.filter(url_slug.eq(query_url_slug)),
+        _ => (),
+    }
+    query.first(conn)
+}
+
+pub fn read_routes(
+    conn: &PgConnection,
+    option_id: Option<i32>,
+    option_parent: Option<Option<i32>>,
+    option_url_slug: Option<String>,
+) -> QueryResult<Vec<Route>> {
+    let mut query = routes.into_boxed();
+    match option_id {
+        Some(query_id) => query = query.filter(id.eq(query_id)),
+        _ => (),
+    }
+    match option_parent {
+        Some(query_parent) => query = query.filter(parent.eq(query_parent)),
+        _ => (),
+    }
+    match option_url_slug {
+        Some(query_url_slug) => query = query.filter(url_slug.eq(query_url_slug)),
+        _ => (),
+    }
+    query.get_results(conn)
+}
+
+//Update
+
+pub fn update_route(
+    conn: &PgConnection,
+    route_id: i32,
+    option_parent: Option<Option<i32>>,
+    option_url_slug: Option<String>,
+) -> Option<QueryResult<usize>> {
+    match read_route(conn, Some(route_id), None, None) {
+        Ok(query_role) => {
+            let mut final_parent: Option<i32> = query_role.parent;
+            let mut final_url_slug: String = query_role.url_slug.clone();
+
+            match option_parent {
+                Some(query_parent) => final_parent = query_parent,
+                _ => (),
+            }
+            match option_url_slug {
+                Some(query_url_slug) => final_url_slug = query_url_slug,
+                _ => (),
+            }
+
+            Some(
+                diesel::update(&query_role)
+                    .set((
+                        parent.eq(final_parent),
+                        url_slug.eq(final_url_slug),
+                    ))
+                    .execute(conn),
+            )
+        }
+        _ => None,
+    }
+}
+
+//Delete
+
+pub fn delete_route(conn: &PgConnection, route_id: i32) -> QueryResult<Route> {
+    match read_route(conn, Some(route_id), None, None) {
+        Ok(query_route) => diesel::delete(&query_route).get_result(conn),
+        Err(e) => Err(e),
+    }
 }
